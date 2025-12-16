7d7139bd2c0bd710f98fbe317bcc51e985307f3e
beebi-k <kalagotlabeebi95@gmail.com>
2025-12-16 20:41:49 +0530

    Q2: Multi component page with Navbar Content Footer

---
+++ 3_structure_components/q2_multi_components/q2_multi_component/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div style={{ fontSize: "14px", color: "gray", padding: "10px", textAlign: "center" }}>
+      <p>This is Footer</p>
+    </div>
+  );
+}
+
+export default Footer;
