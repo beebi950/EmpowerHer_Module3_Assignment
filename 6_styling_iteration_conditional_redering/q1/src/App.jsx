fc575e2a8a29ce1d75cb9dbc35b44a7681ac6746
beebi-k <kalagotlabeebi95@gmail.com>
2025-12-18 13:12:55 +0530

    Q1: Conditional Rendering — Status Toggle Component

---
+++ 6_styling_iteration_conditional_redering/q1/src/App.jsx
@@ -0,0 +1,11 @@
+import StatusToggle from "./StatusToggle";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <StatusToggle />
+    </div>
+  );
+}
+
+export default App;
